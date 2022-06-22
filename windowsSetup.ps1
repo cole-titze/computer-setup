8a5c2d1d064bdb0433debd67b4c5c6f91cba5429
Cole <cole.titze@outlook.com>
2022-06-22 15:29:37 -0500

    removed itunes since it is not working

--- windowsSetup.ps1
+++ windowsSetup.ps1
@@ -5,8 +5,7 @@ winget install Microsoft.VisualStudio.2022.Community
 winget install Git.Git
 # Go through githubs adding ssh key tutorial
 winget install JetBrains.PyCharm.Community
-winget install Apple.iTunes
-winget install Apple.iTunes     # had to run twice
+# winget install Apple.iTunes          // Not working
 # winget install LogMeIn.LastPass      // Not working
 winget install Python.Python.3
 
