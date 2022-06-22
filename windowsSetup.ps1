f41a471574342edcfcdc2d2fede8248a13a67291
Cole <cole.titze@outlook.com>
2022-06-21 21:07:15 -0700

    Added powershell script with winget commands to install wanted software

---
+++ windowsSetup.ps1
@@ -0,0 +1,10 @@
+# Add last-pass extension after download
+winget install Mozilla.Firefox
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.VisualStudio.2022.Community
+winget install Git.Git
+# Go through githubs adding ssh key tutorial
+winget install JetBrains.PyCharm.Community
+winget install Apple.iTunes
+winget install Apple.iTunes     # had to run twice
+# winget install LogMeIn.LastPass      // Not working
