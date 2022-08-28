b68bc0f187dce942baea0137ae3a788625ee7b8c
Cole Titze <cole.titze@outlook.com>
2022-08-28 18:57:59 -0500

    Added ssh keygen back to rpi Script

--- Rpi/rpiSetup.sh
+++ Rpi/rpiSetup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Install dotnet
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current
 echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
 echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
 source ~/.bashrc
+# Create ssh key
+ssh-keygen -t ed25519 -C "cole.titze@outlook.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
