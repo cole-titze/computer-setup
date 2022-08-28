ca86b6db8c58d2e71954675e0194067eab2bd3a0
Cole Titze <cole.titze@outlook.com>
2022-08-28 13:46:38 -0500

    Removed keygen as pi won't push code

--- Rpi/rpiSetup.sh
+++ Rpi/rpiSetup.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 sudo apt install code
-#sudo snap install pycharm-community --classic       // no arm build
-ssh-keygen -t ed25519 -C "cole.titze@outlook.com"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
