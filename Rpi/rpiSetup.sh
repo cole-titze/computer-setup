244ce88f890cf96479d026a2587cd3cc69bca1cb
Cole Titze <cole.titze@outlook.com>
2022-08-28 18:52:34 -0500

    Removed code from install

--- Rpi/rpiSetup.sh
+++ Rpi/rpiSetup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt install code
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current
 echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
 echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
