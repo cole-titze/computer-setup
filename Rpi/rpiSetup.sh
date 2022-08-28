a5b5c7b685ab0ad3d8ead6dc884edcdf164321c3
Cole Titze <cole.titze@outlook.com>
2022-08-28 13:45:00 -0500

    Update packages for pi

--- Rpi/rpiSetup.sh
+++ Rpi/rpiSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo snap install --classic code
-sudo snap install pycharm-community --classic
+sudo apt install code
+#sudo snap install pycharm-community --classic       // no arm build
 ssh-keygen -t ed25519 -C "cole.titze@outlook.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
