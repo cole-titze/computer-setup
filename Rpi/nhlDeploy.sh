9ef4c262f06b47bd8bb468d494f6af9776c12f22
Cole Titze <cole.titze@outlook.com>
2022-08-28 19:09:56 -0500

    Update deploy scripts

--- Rpi/nhlDeploy.sh
+++ Rpi/nhlDeploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-~/source/repos git clone git@github.com:cole-titze/nhl-player-gatherer.git
-~/source/repos git clone git@github.com:cole-titze/nhl-data-gatherer.git
-~/source/repos git clone git@github.com:cole-titze/nhl-odds-web-backend.git
-~/ git clone git@github.com:cole-titze/Releases.git
+cd source/repos
+git clone git@github.com:cole-titze/nhl-player-gatherer.git
+git clone git@github.com:cole-titze/nhl-data-gatherer.git
+git clone git@github.com:cole-titze/nhl-odds-web-backend.git
+cd
+git clone git@github.com:cole-titze/Releases.git
