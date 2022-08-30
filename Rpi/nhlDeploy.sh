82ce0f60a29c1482340733177f3f027725efaf4e
Cole Titze <cole.titze@outlook.com>
2022-08-29 20:57:29 -0500

    Added vegas odds getter and pyodbc

--- Rpi/nhlDeploy.sh
+++ Rpi/nhlDeploy.sh
@@ -3,6 +3,7 @@ cd ~/source/repos
 git clone git@github.com:cole-titze/nhl-player-gatherer.git
 git clone git@github.com:cole-titze/nhl-data-gatherer.git
 git clone git@github.com:cole-titze/nhl-odds-web-backend.git
+git clone git@github.com:cole-titze/VegasOddsGetter.git
 cd
 git clone git@github.com:cole-titze/Releases.git
 cd Releases/Scripts && chmod u+r+x *
--- Rpi/rpiSetup.sh
+++ Rpi/rpiSetup.sh
@@ -4,6 +4,13 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Curre
 echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
 echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
 source ~/.bashrc
+# Install pyodbc
+sudo apt-get install python3-dev unixodbc-dev
+git clone https://github.com/mkleehammer/pyodbc
+cd pyodbc
+python3 setup.py build
+sudo python3 setup.py install
+cd && rm -rf pyodbc
 # Create ssh key
 ssh-keygen -t ed25519 -C "cole.titze@outlook.com"
 eval "$(ssh-agent -s)"
