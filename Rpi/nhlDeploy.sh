0b2566efa99a8afbcda39806712053eaf0519c49
Cole Titze <cole.titze@outlook.com>
2023-06-21 19:55:11 -0500

    Updated file paths

--- Rpi/nhlDeploy.sh
+++ Rpi/nhlDeploy.sh
@@ -3,5 +3,8 @@
 cd
 git clone git@github.com:cole-titze/releases.git
 cd ~/releases/Scripts && chmod u+r+x *
+cd ~/releases/Scripts/containers && chmod u+r+x *
+cd ~/releases/Scripts/nhl && chmod u+r+x *
+
 # Copy env files
 cp -r ~/computer-setup/Rpi/secrets ~/
