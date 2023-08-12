c5487efa2cb07fa417846080180541b784cf5d54
cole-titze <95884122+cole-titze@users.noreply.github.com>
2023-08-12 14:05:34 -0500

    Update windowsSetup.ps1

--- Windows/windowsSetup.ps1
+++ Windows/windowsSetup.ps1
@@ -1,13 +1,16 @@
 # Add last-pass extension after download
 winget install Mozilla.Firefox
+winget install -e --id Nvidia.GeForceExperience
+winget install Valve.Steam
 winget install Microsoft.VisualStudioCode
 winget install Microsoft.AzureDataStudio
 winget install Microsoft.VisualStudio.2022.Community
 winget install Git.Git
 # Go through githubs adding ssh key tutorial
 winget install JetBrains.PyCharm.Community
-# winget install Apple.iTunes          // Not working
-# winget install LogMeIn.LastPass      // Not working
+winget install Spotify.Spotify
+winget install Apple.iTunes          // Not working
+winget install LogMeIn.LastPass      // Not working
 winget install Python.Python.3
 
 # Add git config
