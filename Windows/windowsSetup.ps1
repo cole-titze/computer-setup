cf176be8727ebd9de0c567587cf33c470365c25a
cole-titze <95884122+cole-titze@users.noreply.github.com>
2023-08-12 14:06:15 -0500

    Update windowsSetup.ps1

--- Windows/windowsSetup.ps1
+++ Windows/windowsSetup.ps1
@@ -1,6 +1,6 @@
 # Add last-pass extension after download
 winget install Mozilla.Firefox
-winget install -e --id Nvidia.GeForceExperience
+winget install Nvidia.GeForceExperience
 winget install Valve.Steam
 winget install Microsoft.VisualStudioCode
 winget install Microsoft.AzureDataStudio
@@ -9,12 +9,14 @@ winget install Git.Git
 # Go through githubs adding ssh key tutorial
 winget install JetBrains.PyCharm.Community
 winget install Spotify.Spotify
-winget install Apple.iTunes          // Not working
-winget install LogMeIn.LastPass      // Not working
 winget install Python.Python.3
 
 # Add git config
 git config --global user.email "cole.titze@outlook.com"
 git config --global user.name "Cole"
+
+# Not working
+winget install Apple.iTunes
+winget install LogMeIn.LastPass
 
 winget upgrade --all
