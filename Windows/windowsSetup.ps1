8e56009394531c4f3293f5cd9835bfac427a278c
cole-titze <95884122+cole-titze@users.noreply.github.com>
2023-08-12 17:44:46 -0700

    Update windowsSetup.ps1

--- Windows/windowsSetup.ps1
+++ Windows/windowsSetup.ps1
@@ -1,15 +1,16 @@
-# Add last-pass extension after download
+# Add last-pass extension after download and disable firefox pwd manager 
 winget install Mozilla.Firefox
 winget install Nvidia.GeForceExperience
 winget install Valve.Steam
 winget install Microsoft.VisualStudioCode
 winget install Microsoft.AzureDataStudio
+# Add .NET under modify of the installer
 winget install Microsoft.VisualStudio.2022.Community
 winget install Git.Git
 # Go through githubs adding ssh key tutorial
 winget install JetBrains.PyCharm.Community
 winget install Spotify.Spotify
-winget install Python.Python.3
+winget install Python3
 
 # Add git config
 git config --global user.email "cole.titze@outlook.com"
@@ -20,3 +21,11 @@ winget install Apple.iTunes
 winget install LogMeIn.LastPass
 
 winget upgrade --all
+
+# Get repos
+git clone git@github.com:cole-titze/nhl-data-getter.git
+git clone git@github.com:cole-titze/nhl-data-cleaner.git
+git clone git@github.com:cole-titze/nhl-odds-web-backend.git
+git clone git@github.com:cole-titze/nhl-odds-web-frontend.git
+git clone git@github.com:cole-titze/nhl-game-predictor.git
+git clone git@github.com:cole-titze/nhl-log-loss-getter.git
