4ad66774ff06ce3c39ba3bcd30eb3949d406ec0f
Cole Titze <cole.titze@outlook.com>
2024-04-25 21:59:13 -0500

    Add pi to docker group

--- Rpi/rpiSetup.sh
+++ Rpi/rpiSetup.sh
@@ -2,9 +2,14 @@
 # Install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+# Add pi to docker group
+sudo groupadd docker
+sudo usermod -aG docker pi
+newgrp docker
+
 # Setup portainer
-sudo docker volume create portainer_data
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 # Create ssh key
 ssh-keygen -t ed25519 -C "cole.titze@outlook.com"
 eval "$(ssh-agent -s)"
