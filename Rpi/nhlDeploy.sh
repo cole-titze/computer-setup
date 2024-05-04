efe1894b5f65255d60fc665a927feae3c145e8b0
Cole Titze <cole.titze@outlook.com>
2024-05-04 10:35:25 -0500

    Update directory

--- Rpi/nhlDeploy.sh
+++ Rpi/nhlDeploy.sh
@@ -2,9 +2,9 @@
 # Create release directory
 cd
 git clone git@github.com:cole-titze/releases.git
-cd ~/releases/Scripts && chmod u+r+x *
-cd ~/releases/Scripts/containers && chmod u+r+x *
-cd ~/releases/Scripts/nhl && chmod u+r+x *
+cd ~/releases/scripts && chmod u+r+x *
+cd ~/releases/scripts/containers && chmod u+r+x *
+cd ~/releases/scripts/nhl && chmod u+r+x *
 
 # Copy env files
 cp -r ~/computer-setup/Rpi/secrets ~/
