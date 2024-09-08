79a09347b2ff9043771c655aecf62ac873cf5a98
cole-titze <95884122+cole-titze@users.noreply.github.com>
2024-09-08 14:05:35 -0500

    Update nhl-deploy.sh

--- rpi/scripts/nhl/nhl-deploy.sh
+++ rpi/scripts/nhl/nhl-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Give scripts executable permissions
-cd ~/computer-setup/rpi/scripts && chmod -r u+r+x *
+cd ~/computer-setup/rpi/scripts && chmod -R u+r+x *
 
 # Copy env files
 cp -r ~/computer-setup/rpi/secrets ~/
