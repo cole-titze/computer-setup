edb4805115e5b3098fb9baa7822e0201741cae1a
Cole Titze <cole.titze@outlook.com>
2024-09-08 14:16:19 -0500

    Update where containers pull from

--- rpi/scripts/containers/llm-web-backend.sh
+++ rpi/scripts/containers/llm-web-backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker stop -t 600 llm-web-backend
 docker rm llm-web-backend
-docker pull ghcr.io/cole-titze/llm-web-backend:latest
-docker run -d --name=llm-web-backend  --restart=always --volume=/var/llm-models:/llm/models --memory=5.5GB --cpus=3.33 --publish=8083:8083 ghcr.io/cole-titze/llm-web-backend:latest
+docker pull ghcr.io/cole-titze/containers/llm-web-backend:latest
+docker run -d --name=llm-web-backend  --restart=always --volume=/var/llm-models:/llm/models --memory=5.5GB --cpus=3.33 --publish=8083:8083 ghcr.io/cole-titze/containers/llm-web-backend:latest
--- rpi/scripts/containers/magic-mirror.sh
+++ rpi/scripts/containers/magic-mirror.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker stop magic-mirror
 docker rm magic-mirror
-docker pull ghcr.io/cole-titze/magic-mirror-wrapper:latest
-docker run --name magic-mirror -p 8082:8082 --restart=always -d ghcr.io/cole-titze/magic-mirror-wrapper:latest
+docker pull ghcr.io/cole-titze/containers/magic-mirror:latest
+docker run --name magic-mirror -p 8082:8082 --restart=always -d ghcr.io/cole-titze/containers/magic-mirror:latest
